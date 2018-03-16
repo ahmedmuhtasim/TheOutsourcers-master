fdcffc1499867ccd1d18f31d37bab62535e4a3bb
Evan Typanski <evantypanski@gmail.com>
2018-03-16 14:19:57 -0400

    Fixed preliminary models syntax

--- outsourcers/models.py
+++ outsourcers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Office(models.Model):
-    title = models.charField(max_length=30)
+    title = models.CharField(max_length=30)
     term = models.IntegerField(max_length=2)
     AOG = (
         ('F', 'Federal'),
@@ -17,28 +17,30 @@ class Office(models.Model):
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    SSN = models.IntegerField(min_length=9)
+    SSN = models.IntegerField(max_length=9)
     federal_district = models.IntegerField(max_length=2)
     state_district = models.IntegerField(max_length=3)
 
 
 class Voter(models.Model):
-    person = models.OneToOneField(Person)
-    ballots = models.ManyToManyField(Ballots)
+    # on_delete tells what to do if person is deleted - in that case, do SQL CASCADE
+    # This guarantees that a Voter will have a person, it won't become null
+    # Default null value for django one to one field is false, so person will never be false
+    person = models.OneToOneField(Person, on_delete=models.CASCADE) 
+    #ballots = models.ManyToManyField(Ballots)
 
 
-class Politician(models.Model):
-    person = models.OneToOneField(Person)
-    candidacies = models.ManyToManyField(Candidacy)
-
-
-class Candidacy(mdoels.Model):
+class Candidacy(models.Model):
     politician = models.OneToOneField(Politician)
     PARTY = (
         ('D', 'Democrat'),
         ('R', 'Republican'),
         ('I', 'Independent'),
     )
-    party_affiliation = models.CharField(max_length=1, choices=PARTY)
+    party_affiliation = models.CharField(max_length=3, choices=PARTY)
     office = models.OneToOneField(Office)
 
+
+class Politician(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    candidacies = models.ManyToManyField(Candidacy)
