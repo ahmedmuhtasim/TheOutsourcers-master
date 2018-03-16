42cd7b050fb4855b75d90b07591a274f5c937169
Evan Typanski <evantypanski@gmail.com>
2018-03-16 14:37:50 -0400

    Fixed more model relationship errors

--- outsourcers/models.py
+++ outsourcers/models.py
@@ -3,23 +3,23 @@ from django.db import models
 
 class Office(models.Model):
     title = models.CharField(max_length=30)
-    term = models.IntegerField(max_length=2)
+    term = models.IntegerField()
     AOG = (
         ('F', 'Federal'),
         ('S', 'State'),
         ('L', 'Local'),
     )
-    area_of_governance = models.CharField(max_length=1, choices=AOG)
-    federal_district = models.IntegerField(max_length=2)
-    state_district = models.IntegerField(max_length=3)
+    area_of_governance = models.CharField(max_length=30, choices=AOG)
+    federal_district = models.IntegerField()
+    state_district = models.IntegerField()
 
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    SSN = models.IntegerField(max_length=9)
-    federal_district = models.IntegerField(max_length=2)
-    state_district = models.IntegerField(max_length=3)
+    SSN = models.CharField(max_length=9)
+    federal_district = models.IntegerField()
+    state_district = models.IntegerField()
 
 
 class Voter(models.Model):
@@ -30,17 +30,19 @@ class Voter(models.Model):
     #ballots = models.ManyToManyField(Ballots)
 
 
+class Politician(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+
 class Candidacy(models.Model):
-    politician = models.OneToOneField(Politician)
+    politician = models.OneToOneField(Politician, on_delete=models.CASCADE)
     PARTY = (
         ('D', 'Democrat'),
         ('R', 'Republican'),
         ('I', 'Independent'),
     )
     party_affiliation = models.CharField(max_length=3, choices=PARTY)
-    office = models.OneToOneField(Office)
+    office = models.OneToOneField(Office, on_delete=models.CASCADE)
 
-
-class Politician(models.Model):
-    person = models.OneToOneField(Person, on_delete=models.CASCADE)
-    candidacies = models.ManyToManyField(Candidacy)
+class Ballot(models.Model):
+    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
