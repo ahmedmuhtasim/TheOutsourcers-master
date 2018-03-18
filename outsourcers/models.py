0d38d8d8a6ff7cbc4ceb07f031e994d141bb09b4
Evan Typanski <evantypanski@gmail.com>
2018-03-18 15:50:24 -0400

    Added Ballot model and minor fixes

--- outsourcers/models.py
+++ outsourcers/models.py
@@ -9,7 +9,7 @@ class Office(models.Model):
         ('S', 'State'),
         ('L', 'Local'),
     )
-    area_of_governance = models.CharField(max_length=30, choices=AOG)
+    area_of_governance = models.CharField(max_length=1, choices=AOG)
     federal_district = models.IntegerField()
     state_district = models.IntegerField()
 
@@ -27,14 +27,23 @@ class Voter(models.Model):
     # This guarantees that a Voter will have a person, it won't become null
     # Default null value for django one to one field is false, so person will never be false
     person = models.OneToOneField(Person, on_delete=models.CASCADE) 
-    #ballots = models.ManyToManyField(Ballots)
+    voting_eligible = models.BooleanField(default=True)
 
 
 class Politician(models.Model):
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
 
 
+class Measure(models.Model):
+    MEASURE_TYPES = (
+            ('R', 'Referendum'),
+            ('C', 'Candidacy')
+    )
+    measure_type = models.CharField(max_length=1, choices=MEASURE_TYPES)
+
+
 class Candidacy(models.Model):
+    measure = models.OneToOneField(Measure, on_delete=models.CASCADE)
     politician = models.OneToOneField(Politician, on_delete=models.CASCADE)
     PARTY = (
         ('D', 'Democrat'),
@@ -43,6 +52,27 @@ class Candidacy(models.Model):
     )
     party_affiliation = models.CharField(max_length=3, choices=PARTY)
     office = models.OneToOneField(Office, on_delete=models.CASCADE)
+    # Votes are tallied in this model for ease of tallying and lack of tracking
+    votes = models.IntegerField(default=0)
+
+
+class Referendum(models.Model):
+    measure = models.OneToOneField(Measure, on_delete=models.CASCADE)
+    question = models.TextField(blank=True)
+    # Choices are linked in the Choice model
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Referendum, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    # Votes are tallied in this model for ease of tallying and lack of tracking
+    votes = models.IntegerField(default=0)
+
 
 class Ballot(models.Model):
     voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    # Votes are NOT stored in ballot - thinking they are read then a choice is updated
+    # This avoids being able to trace the ballot - the ballot simply puts many candidacies
+    # and referendums in a single place and assigns a voter to this ballet
+    # It might also work with many to many, many voters per ballot and many ballots per voter
+
