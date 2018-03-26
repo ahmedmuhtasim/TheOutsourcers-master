13e6d3bf233208ab04168656ec46b8a1353fed41
The-Jackrabbit <lsm5fm@virginia.edu>
2018-03-26 16:26:18 -0400

    Fixed Static Urls

--- outsourcers/settings.py
+++ outsourcers/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/code/static/'
+STATIC_URL = '/code/app/static/'
+STATIC_ROOT = '/'
 MEDIA_URL = '/pics/'
 
 MEDIA_ROOT = BASE_DIR
--- outsourcers/urls.py
+++ outsourcers/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
+	url('admin/', admin.site.urls),
 	url(r'^$', views.home),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
