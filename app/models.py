e0295ef792695f81e1d2d659cbbebdbd9188a855
Evan Typanski <evantypanski@gmail.com>
2018-03-26 16:45:26 -0400

    Updated models for elections

--- app/models.py
+++ app/models.py
@@ -22,30 +22,17 @@ class Person(models.Model):
     state_district = models.IntegerField()
 
 
-class Voter(models.Model):
-    # on_delete tells what to do if person is deleted - in that case, do SQL CASCADE
-    # This guarantees that a Voter will have a person, it won't become null
-    # Default null value for django one to one field is false, so person will never be false
-    person = models.OneToOneField(Person, on_delete=models.CASCADE)
-    voter_number = models.IntegerField()
-    STATUS_TYPES = (
-        ('A', 'Active'),
-        ('I', 'Inactive')
-    )
-    voter_status = models.CharField(max_length=1,
-    choices = STATUS_TYPES)
-    date_registered = models.CharField(max_length=10)
-    street_address = models.CharField(max_length=40)
-    city = models.CharField(max_length=30)
-    state = models.CharField(max_length=2)
-    zip = models.IntegerField(max_length=5)
-    locality = models.CharField(max_length=40)
-
-    voting_eligible = models.BooleanField(default=True)
-
-
 class Politician(models.Model):
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+
+
+class Ballot(models.Model):
+    pass
+    # Votes are NOT stored in ballot - thinking they are read then a choice is updated
+    # This avoids being able to trace the ballot - the ballot simply puts many candidacies
+    # and referendums in a single place and assigns a voter to this ballet
+    # It might also work with many to many, many voters per ballot and many ballots per voter
 
 
 class Measure(models.Model):
@@ -54,6 +41,7 @@ class Measure(models.Model):
             ('C', 'Candidacy')
     )
     measure_type = models.CharField(max_length=1, choices=MEASURE_TYPES)
+    ballot = models.ManyToManyField(Ballot)
 
 
 class Candidacy(models.Model):
@@ -83,21 +71,40 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 
-class Ballot(models.Model):
-    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
-    # Votes are NOT stored in ballot - thinking they are read then a choice is updated
-    # This avoids being able to trace the ballot - the ballot simply puts many candidacies
-    # and referendums in a single place and assigns a voter to this ballet
-    # It might also work with many to many, many voters per ballot and many ballots per voter
-
 class Precinct(models.Model):
     name = models.CharField(max_length=250)
-    id = models.IntegerField(max_length=4, primary_key=True)
+    id = models.CharField(max_length=4, primary_key=True)
 
 
 class Poll_Worker(models.Model):
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
     precinct = models.OneToOneField(Precinct, on_delete=models.CASCADE)
+
+
+class Voter(models.Model):
+    # on_delete tells what to do if person is deleted - in that case, do SQL CASCADE
+    # This guarantees that a Voter will have a person, it won't become null
+    # Default null value for django one to one field is false, so person will never be false
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    voter_number = models.IntegerField()
+    STATUS_TYPES = (
+        ('A', 'Active'),
+        ('I', 'Inactive')
+    )
+    voter_status = models.CharField(max_length=1,
+    choices = STATUS_TYPES)
+    date_registered = models.CharField(max_length=10)
+    street_address = models.CharField(max_length=40)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5, null=True)
+    locality = models.CharField(max_length=40)
+    precinct = models.OneToOneField(Precinct, on_delete=models.SET_NULL, null=True)
+    # Can they vote? The voter must check in - once they check in, voting_eligible will become True
+    # When they cannot vote - so they have not checked in/election over, should be False
+    # voting_eligible should be made false with a ballot submission
+    voting_eligible = models.BooleanField(default=False)    
+
 
 class Election(models.Model):
     id = models.CharField(max_length=7, primary_key=True)
