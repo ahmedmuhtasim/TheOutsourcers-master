f5c326a2a67f0fb38c65ddcd6ab32eaa6e283b8c
Evan Typanski <evantypanski@gmail.com>
2018-04-15 20:52:40 -0400

    Added I voted sticker to receipt

--- host_scripts/printer.py
+++ host_scripts/printer.py
@@ -1,57 +1,60 @@
 from flask import Flask, request
 from escpos.printer import Usb
+import os
+
 app = Flask(__name__)
 p = Usb(0x416, 0x5011)
 
 def encode(location, time, id):							#method to send relevant data to the printer
-														#we need to set sizes for each of the 3 parts (sum of 3 must be 12 digits). Current configuration: loc: 3, tim: 3, id: 6
-		list = []										#array of size	13
+													#we need to set sizes for each of the 3 parts (sum of 3 must be 12 digits). Current configuration: loc: 3, tim: 3, id: 6
+	list = []										#array of size	13
 
-		list.append((location//100)%10)				   #keep doing as many times as needed
-		list.append((location//10)%10)
-		list.append(location%10)
-		
-		list.append((time//100)%10)		
-		list.append((time//10)%10)
-		list.append(time%10)
-		
-		list.append((id//100000)%10)	
-		list.append((id//10000)%10)
-		list.append((id//1000)%10)
-		list.append((id//100)%10)		
-		list.append((id//10)%10)
-		list.append(id%10)
+	list.append((location//100)%10)				   #keep doing as many times as needed
+	list.append((location//10)%10)
+	list.append(location%10)
+	
+	list.append((time//100)%10)		
+	list.append((time//10)%10)
+	list.append(time%10)
+	
+	list.append((id//100000)%10)	
+	list.append((id//10000)%10)
+	list.append((id//1000)%10)
+	list.append((id//100)%10)		
+	list.append((id//10)%10)
+	list.append(id%10)
 
-		sum = 0											#keep track of sum
-		
-		for i in range(1, 12, 2):
-				sum += list[i]
-				
-		sum *= 3
-		
-		for i in range(0, 11, 2):
-				sum += list[i]	
-				
-						
-		list.append(((((sum//10)+1)*10)-sum)%10)	   #append parity bit
+	sum = 0											#keep track of sum
+	
+	for i in range(1, 12, 2):
+			sum += list[i]
+			
+	sum *= 3
+	
+	for i in range(0, 11, 2):
+			sum += list[i]	
+			
+					
+	list.append(((((sum//10)+1)*10)-sum)%10)	   #append parity bit
 
-		string = ''									   #string to return
-		
-		for i in range(0, len(list)):
-				string += str(list[i])
+	string = ''									   #string to return
+	
+	for i in range(0, len(list)):
+			string += str(list[i])
 
-		return string
+	return string
 
 def decode(string):									   #method to extract info from scanned barcode
-		num = int(string)
-		num //= 10									   #get rid of parity
-		
-		id = num%1000000
-		time = (num//1000000)%1000
-		location = (num//1000000000)%1000
+	num = int(string)
+	num //= 10									   #get rid of parity
+	
+	id = num%1000000
+	time = (num//1000000)%1000
+	location = (num//1000000000)%1000
 
 
-		return location, time, id
+	return location, time, id
+
 @app.route('/voternumber', methods=['POST'])
 def result():
 	p.text(' ' + request.form['voter'])
@@ -66,6 +69,7 @@ def votedfor():
 			print(key, ":", value)
 			p.text(' ' + key + ": " + value + '\n')
 
+	p.image("voted.jpg")
 	p.cut()
 	return 'Received a ballot!' # response to your request.
 
