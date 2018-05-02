27fe48d577976fe2c5be256ff21d13eb6f91b44e
Evan Typanski <evantypanski@gmail.com>
2018-05-01 23:33:55 -0400

    Debug to false

--- outsourcers/settings.py
+++ outsourcers/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%!gl@t6_tox_&rw#)75d)scqwx%o6@a$ufxx5ypr6$+@03nifz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
